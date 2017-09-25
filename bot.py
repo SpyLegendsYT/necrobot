98d1fa4c3cff04ff5c6ee67ca2f837ccc227edba
Clement <clement.julia13@gmail.com>
2017-09-25 16:58:47 +0300

    plz work

--- bot.py
+++ bot.py
@@ -63,13 +63,7 @@ replyList = [
 
 #forgive me gods of Python
 def startswith_prefix(message):
-    if message.content.startswith(tuple(prefixes)):
-        return True
-
-    if serverData[message.server.id]["prefix"] != "" and message.content.startswith(serverData[message.server.id]["prefix"]):
-        return True
-
-    return False
+    return message.content.startswith(tuple(prefixes)) or (serverData[message.server.id]["prefix"] != "" and message.content.startswith(serverData[message.server.id]["prefix"]))
 
 def is_spam(message):
     userID = userData[message.author.id]
@@ -77,27 +71,20 @@ def is_spam(message):
     if serverData[message.server.id]["automod"] == "":
         return False
 
-    if any([userID, channelID] in serverData[message.server.id]["ignoreAutomod"]):
-        return False
-
-    if startswith_prefix(message):
-        return False
-
-    if message.content.lower() == userID['lastMessage'].lower() and userID['lastMessageTime'] > c.timegm(t.gmtime()) + 2:
-        return True
-
-    if userID['lastMessageTime'] > c.timegm(t.gmtime()) + 1:
-        return True
-
+    return ((message.content.lower() == userID['lastMessage'].lower() and userID['lastMessageTime'] > c.timegm(t.gmtime()) + 2) or userID['lastMessageTime'] > c.timegm(t.gmtime()) + 1) and (userID not in serverData[message.server.id]["ignoreAutomod"] and channelID not in serverData[message.server.id]["ignoreAutomod"]) and not startswith_prefix(message)
 
 def is_allowed_summon(message):
     userID = message.author.id
     channelID = message.channel.id
+<<<<<<< HEAD
     if serverData[userID]["perms"][message.server.id] >= 4:
         return True
 
     if any([userID, channelID] in serverData[message.server.id]["ignoreCommand"]):
         return False
+=======
+    return ((channelID not in serverData[message.server.id]["ignoreCommand"] and message.author.id not in serverData[message.server.id]["ignoreCommand"]) or userID["perms"][message.server.id] >= 4) and startswith_prefix(message)
+>>>>>>> parent of bd1e5c2... trying for webpage
 
 def logit(message):
     if startswith_prefix(message):
